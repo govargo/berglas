cf7b29cc83547e331ce4ca1cf92a4a30b167d521
Iso Kenta <k-iso@colopl.co.jp>
2019-10-23 11:55:04 +0900

    validation

---
+++ examples/validation/main.go
@@ -0,0 +1,98 @@
+package foo
+
+import (
+	"context"
+"fmt"
+"github.com/GoogleCloudPlatform/berglas/pkg/berglas"
+"net/http"
+"os"
+
+kwhhttp "github.com/slok/kubewebhook/pkg/http"
+kwhlog "github.com/slok/kubewebhook/pkg/log"
+kwhvalidating "github.com/slok/kubewebhook/pkg/webhook/validating"
+corev1 "k8s.io/api/core/v1"
+metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type BerglasValidator struct {
+	logger kwhlog.Logger
+}
+
+// Validate implements ValidateFunc and provides the top-level entrypoint for object
+// validation.
+func (v *BerglasValidator) Validate(ctx context.Context, obj metav1.Object) (bool, kwhvalidating.ValidatorResult, error) {
+	v.logger.Infof("calling validate")
+
+	secret, ok := obj.(*corev1.Secret)
+	if !ok {
+		v.logger.Errorf("error happens when cast object to secret")
+		return false, kwhvalidating.ValidatorResult{}, fmt.Errorf("not an ingress")
+	}
+
+	for key, val := range secret.Data {
+		validateError := v.validateSecretData(ctx, val)
+		if validateError {
+			res := kwhvalidating.ValidatorResult{
+				Valid:   false,
+				Message: fmt.Sprintf("the secret data: %s invalid berglas reference. the data cannot be decrypted.", key),
+			}
+			return false, res, nil
+		}
+	}
+
+	v.logger.Infof("secret %s is valid", secret.Name)
+	res := kwhvalidating.ValidatorResult{
+		Valid:   true,
+		Message: "all secretData are valid",
+	}
+	return false, res, nil
+}
+
+func (v *BerglasValidator) validateSecretData(ctx context.Context, data []byte) (bool) {
+	v.logger.Debugf("start validating of secret data")
+	isBerglasReference := v.hasBerglasReferences(data)
+	if !isBerglasReference {
+		v.logger.Infof("this secret resource does not have Barglas Reference.(i.e. berglas://${BUCKET_ID}/api-key)")
+		return false
+	}
+
+	return true
+}
+
+func (v *BerglasValidator) hasBerglasReferences(data []byte) (bool) {
+	decStr := string(data)
+	if berglas.IsReference(decStr) {
+		return true
+	}
+	return false
+}
+
+// webhookHandler is the http.Handler that responds to webhooks
+func webhookHandler() http.Handler {
+	logger := &kwhlog.Std{Debug: true}
+
+	vl := &BerglasValidator{logger: logger}
+
+	vcfg := kwhvalidating.WebhookConfig{
+		Name: "berglasSecrets",
+		Obj:  &corev1.Secret{},
+	}
+
+	// Create the wrapping webhook
+	wh, err := kwhvalidating.NewWebhook(vcfg, vl, nil, nil, logger)
+	if err != nil {
+		logger.Errorf("error creating webhook: %s", err)
+		os.Exit(1)
+	}
+
+	// Get the handler for our webhook.
+	whhandler, err := kwhhttp.HandlerFor(wh)
+	if err != nil {
+		logger.Errorf("error creating webhook handler: %s", err)
+		os.Exit(1)
+	}
+	return whhandler
+}
+
+// F is the exported webhook for the function to bind.
+var F = webhookHandler().ServeHTTP
---
+++ examples/validation/main_test.go
@@ -0,0 +1,90 @@
+package foo
+
+import (
+	"context"
+	kwhlog "github.com/slok/kubewebhook/pkg/log"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+)
+
+
+func Test_Validate_Berglas(t *testing.T) {
+	t.Run("secret resource has berglas reference", func(t *testing.T) {
+
+		expect := false
+		testData := map[string]string{
+			"API_KEY": "berglas://verification-iso-berglas-secret/api-key",
+			"TLS_KEY": "berglas://verification-iso-berglas-secret/tls-key",
+		}
+
+		ctx := context.Background()
+		logger := &kwhlog.Std{Debug: true}
+		validator := &BerglasValidator{logger: logger}
+		secretData := map[string][]byte{}
+
+		for k, _ := range testData {
+			secretData[k] = []byte(testData[k])
+		}
+
+		secret := corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "testsecret",
+				Namespace: "default",
+			},
+			Data:       secretData,
+			StringData: nil,
+			Type:       "",
+		}
+
+		_, result ,err := validator.Validate(ctx, &secret)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if result.Valid != expect {
+			t.Fail()
+		} else {
+			println("Test Sucessed")
+		}
+	})
+}
+
+func Test_Validate_BerglasLess(t *testing.T) {
+	t.Run("secret resource does not have berglas reference", func(t *testing.T) {
+
+		expect := true
+		testData := map[string]string{
+			"API_KEY": "abcd1234",
+			"TLS_KEY": "efgh5678",
+		}
+
+		ctx := context.Background()
+		logger := &kwhlog.Std{Debug: true}
+		validator := &BerglasValidator{logger: logger}
+		secretData := map[string][]byte{}
+
+		for k, v := range testData {
+			secretData[k] = []byte(v)
+		}
+
+		secret := corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "testsecret",
+				Namespace: "default",
+			},
+			Data:       secretData,
+			StringData: nil,
+			Type:       "",
+		}
+
+		_, result, err := validator.Validate(ctx, &secret)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if result.Valid != expect {
+			t.Fail()
+		} else {
+			println("Test Sucessed")
+		}
+	})
+}
